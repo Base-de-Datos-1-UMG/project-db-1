c3842e6d0d6c32aab28f7bae65e69ed222d6c380
OmarPocon94 <114636510+OmarPocon94@users.noreply.github.com>
2022-10-18 22:22:47 -0600

    Add files via upload

---
+++ create-registers/medicamento.php
@@ -0,0 +1,45 @@
+<?php
+
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
+
+	//Incluir el archivo de conexion
+	include_once("./includes/connection.php");
+
+	//Obtenemos el contenido de cada input en el formulario
+	$nombre = $_POST['nombre'];
+	$precio = $_POST['precio'];
+	$lote = $_POST['lote'];
+
+
+	//concatenamos cada variable en el insert
+	$sql = "INSERT INTO MEDICAMENTO (IDMEDICAMENTO,NOMBRE_MEDICAMENTO)
+			VALUES (1, '".$nombre."')";
+
+	$sql3 = "INSERT INTO MEDICAMENTO_X_LABORATORIO (PRECIO_MEDICAMENTO,LOTE_MEDICAMENTO)
+	        VALUES ('".$precio."', '".$lote."')";
+	//se realiza commit
+	$sql2 = "COMMIT";
+
+	//creamos objetos sql
+	$stid = oci_parse($conn, $sql);
+	$stid2 = oci_parse($conn, $sql2);
+	$stid3 = oci_parse($conn, $sql3);
+
+	//ejecutamos los 2 codigos sql
+	oci_execute($stid);
+	oci_execute($stid2);
+	oci_execute($stid3);
+
+	//cerramos conexiones
+	oci_free_statement($stid);
+	oci_free_statement($stid2);
+	oci_free_statement($stid3);
+	oci_close($conn);
+
+	echo $sql;
+
+	//regresar al archivo de ingresar usuario
+	header('Location: ./medicamentos.html');
+
+?>

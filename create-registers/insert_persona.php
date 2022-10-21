6919b326c7881c02bb5870cef4f4db8153f3842a
William <49001552+Andres715@users.noreply.github.com>
2022-10-20 23:27:56 -0600

    Add files via upload

--- create-registers/insert_persona.php
+++ create-registers/insert_persona.php
@@ -8,38 +8,37 @@
 
 	//Obtenemos el contenido de cada input en el formulario
 	$nombre = $_POST['nombre'];
-	$precio = $_POST['precio'];
-	$lote = $_POST['lote'];
-
+	$segnombre = $_POST['segnombre'];
+	$apellido = $_POST['apellido'];
+	$segapellido = $_POST['segapellido'];
+	$nit = $_POST['nit'];
+	$dpi = $_POST['dpi'];
+	$password = $_POST['password'];
+	$carnet = $_POST['carnet'];
+	$genero = $_POST['genero'];
 
 	//concatenamos cada variable en el insert
-	$sql = "INSERT INTO MEDICAMENTO (IDMEDICAMENTO,NOMBRE_MEDICAMENTO)
-			VALUES (1, '".$nombre."')";
-
-	$sql3 = "INSERT INTO MEDICAMENTO_X_LABORATORIO (PRECIO_MEDICAMENTO,LOTE_MEDICAMENTO)
-	        VALUES ('".$precio."', '".$lote."')";
+	$sql = "INSERT INTO PERSONA (IDPERSONA,NOMBRE1,NOMBRE2,APELLIDO1,APELLIDO2,NIT,DPI,GENERO,CARNET)
+			VALUES (IDPERSONA.NEXTVAL, '".$nombre."', '".$segnombre."', '".$apellido."', '".$segapellido."', '".$nit."', '".$dpi."', '".$genero."', '".$carnet."')";
 	//se realiza commit
 	$sql2 = "COMMIT";
 
 	//creamos objetos sql
 	$stid = oci_parse($conn, $sql);
 	$stid2 = oci_parse($conn, $sql2);
-	$stid3 = oci_parse($conn, $sql3);
 
 	//ejecutamos los 2 codigos sql
 	oci_execute($stid);
-	oci_execute($stid3);
 	oci_execute($stid2);
 
 	//cerramos conexiones
 	oci_free_statement($stid);
 	oci_free_statement($stid2);
-	oci_free_statement($stid3);
 	oci_close($conn);
 
 	echo $sql;
 
-	//regresar al archivo de ingresar medicamento
-	header('Location: ../view-registers/medicamentos.html');
+	//regresar al archivo de ingresar usuario
+	header('Location: ./create_user.html');
 
 ?>
